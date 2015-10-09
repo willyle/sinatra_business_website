6827c41c61967a9da6ea42a52f1f6ee9387dc8ed
William Le <mrwillyle@gmail.com>
2015-10-09 16:00:47 -0400

    Working nav bar. not final

--- main.rb
+++ main.rb
@@ -1,32 +1,37 @@
 require "sinatra"
-require "mandrill-api"
 
 get "/" do
 	@title = "Home"
+	@photo = "home"
 	erb :home
 end
 
 get "/about" do
 	@title = "How It Works"
+	@photo = "about"
 	erb :about
 end
 
 get "/services" do
 	@title = "Our Services"
+	@photo = "services"
 	erb :services
 end
 
 get "/contact" do
 	@title = "Contact Us"
+	@photo = "contact"
 	erb :contact
 end
 
 get "/appointment" do
 	@title = "Book An Appointment"
+	@photo = "appointment"
 	erb :appointment
 end
 
 get "/recruit" do
 	@title = "Join Our Team"
+	@photo = "recruit"
 	erb :recruit
 end
---
+++ public/js/javascript.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+	$("li").on("mouseover",function(){
+		$(this).css("background-color", "blue");
+	});
+
+	$("li").on("mouseout",function(){
+		$(this).css("background-color", "gray");
+	});
+
+
+
+
+
+
+
+
+
+
+});

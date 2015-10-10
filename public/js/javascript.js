7047aad3ff1891e3cd5419ab366a21fcfb0daf25
William Le <mrwillyle@gmail.com>
2015-10-10 15:14:04 -0400

    closes #1. added login and signup buttons and boxes. commented the borders around the divs

--- public/js/javascript.js
+++ public/js/javascript.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+	//hovering effect for nav bar
 	$("li").on("mouseover",function(){
 		$(this).css("background-color", "gray");
 	});
@@ -7,13 +8,43 @@ $(document).ready(function(){
 		$(this).css("background-color", "#e9e9e9");
 	});
 
+	//obtaining width of container to move log-in/sign-u[] box to the right side
+	var width,move;
+	window.onresize = window.onload = function() {
+		width = $("#container").innerWidth();
+		move = width-200;
+		console.log(move);
+		$("#login").css({left: move});
+		$("#signup").css({left: move});
+	}
 
+	//display and hide log-in and sign-up
+	var login = false;
+	var signup = false;
 
+	$("#login_button").on("click", function(){
+		if(login === false){
+			login = true;
+			signup = false;
+			$("#login").removeClass("hide").addClass("show");
+			$("#signup").removeClass("show").addClass("hide");
+		}
+		else{
+			login = false;
+			$("#login").removeClass("show").addClass("hide");
+		}
+	});
 
-
-
-
-
-
-
+	$("#signup_button").on("click", function(){
+		if(signup === false){
+			signup = true;
+			login = false;
+			$("#signup").removeClass("hide").addClass("show");
+			$("#login").removeClass("show").addClass("hide");
+		}
+		else{
+			signup = false;
+			$("#signup").removeClass("show").addClass("hide");
+		}
+	});
 });

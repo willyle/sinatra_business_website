d979918a1b7257469e566043d46841ccd51055cb
William Le <mrwillyle@gmail.com>
2015-10-11 21:49:37 -0400

    fixed merge conflict

--- public/js/javascript.js
+++ public/js/javascript.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
-<<<<<<< HEAD
 	$("li").on("mouseover",function(){
 		$(this).css("background-color", "grey");
 	});
 
 	$("li").on("mouseout",function(){
 		$(this).css("background-color", "#e9e9e9");
-=======
+	});
+
 	//hovering effect for nav bar
 	$("a").on("mouseover",function(){
 		$(this).closest("li").css("background-color", "gray");
@@ -14,10 +14,9 @@ $(document).ready(function(){
 
 	$("a").on("mouseout",function(){
 		$(this).closest("li").css("background-color", "#e9e9e9");
->>>>>>> 92ba35182e15c2b7661ea86f1a02b479694d1ea5
 	});
 
-	//obtaining width of container to move log-in/sign-u[] box to the right side
+	//obtaining width of container to move log-in/sign-up box to the right side
 	var width,move;
 	window.onresize = window.onload = function() {
 		width = $("#container").innerWidth();

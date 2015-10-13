83fea96972bd91743da4f09c78897a6a09b70a28
jesus moreno <loljmtorres@gmail.com>
2015-10-12 20:24:12 -0400

    fourth

--- mail.rb
+++ mail.rb
@@ -3,7 +3,7 @@ api_key = ENV['MANDRILL_APIKEY']
 m = Mandrill::API.new "#{api_key}"
 message = {
  :subject=> "Hello from the Mandrill API",
- :from_name=> "Your name",
+ :from_name=> "TechTo-Me",
  :text=>"Hi message, how are you?",
  :to=>[{:email=> "loljmtorres@gmail.com", :name=> "Recipient1"}],
  :html=>"<html><h1>Hi <strong>message</strong>, how are you?</h1></html>",
